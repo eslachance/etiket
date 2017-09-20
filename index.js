9823aaa69cf5b8f57ad6d8cba2f02cd45f2753bb
Evelyne Lachance <eslachance@gmail.com>
2017-09-20 16:40:40 -0400

    Whoops I guess the message cache can't be that small

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
   disableEveryone: true,
   disabledEvents: config.disabledEvents,
-  messageCacheMaxSize: 0,
+  messageCacheMaxSize: 2,
   messageCacheLifetime: 1
 });
 
